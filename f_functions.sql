c86a3c4107944a3aa43069821d52f49b0caa14e1
dannaa <ykuremysun@gmail.com>
2026-09-01 12:59:42 -0500

    fix: 🍕 functions

--- f_functions.sql
+++ f_functions.sql
@@ -1,29 +1,20 @@
 USE thepizzeria_don_piccolo;
 
 -- Función para calcular el total de un pedido (sumando precios de pizzas + costo de envío + IVA).
-DROP FUNCTION IF EXISTS f_calcular_total_pedido;
 DELIMITER $$
-CREATE FUNCTION f_calcular_total_pedido(p_id_pedido INT)
-RETURNS DOUBLE
-NOT DETERMINISTIC
-READS SQL DATA
+CREATE FUNCTION calcular_total_pedido_fah(p_id_pedido INT)
+RETURNS double
+DETERMINISTIC
 BEGIN
-  DECLARE v_subtotal DOUBLE DEFAULT 0;
-  DECLARE v_envio DOUBLE DEFAULT 0;
-  DECLARE v_total DOUBLE DEFAULT 0;
- -- SI LA SUMA DE LAS CANTIDADES * PRECIOS UNITARIOS ES NULL, PONER 0
-  SELECT IFNULL(SUM(cantidad * precio_unitario), 0) INTO v_subtotal
-  FROM detallepedido
-  WHERE id_pedido = p_id_pedido;
- -- BUSCAR EL COSTO DEL ENVIO CORRESPONDE AL PEDIDO
-  SELECT IFNULL(costo_envio, 0) INTO v_envio
-  FROM domicilio
-  WHERE id_pedido = p_id_pedido;
- -- SE SUMA EL SUBTOTAL Y ENVIO, MULTIPLICAR POR IVA Y EL RESULTADO QUEDA CON 2 DECIMALES
-  SET v_total = ROUND((v_subtotal + v_envio) * 1.19, 2);
-  RETURN v_total;
-END$$
-DELIMITER ;
+DECLARE v_costo_pizza DOUBLE DEFAULT 0;
+DECLARE v_costo_domicilio DOUBLE DEFAULT 0;
+DECLARE v_costo_total DOUBLE DEFAULT 0;
+	select subtotal into v_costo_pizza FROM pedido WHERE id_pedido=p_id_pedido;
+    select costo_envio into v_costo_domicilio FROM domicilio WHERE id_pedido=p_id_pedido;
+    set v_costo_total=ROUND((v_costo_pizza+v_costo_domicilio)*1.19,2);
+RETURN v_costo_total;
+END; $$
+DELIMITER ; 
 
 -- Función para calcular la ganancia neta diaria (ventas - costos de ingredientes).
 DROP FUNCTION IF EXISTS f_ganancia_neta_diaria;
